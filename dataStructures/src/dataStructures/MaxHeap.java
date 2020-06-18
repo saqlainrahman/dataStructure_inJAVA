a82f24fce3fe3aea4a64e0db76d61a42b834f91e
user <user@DESKTOP-PBRLDCP>
2020-06-18 14:49:47 +0530

    max.heap

---
+++ dataStructures/src/dataStructures/MaxHeap.java
@@ -0,0 +1,71 @@
+package dataStructures;
+
+public class MaxHeap {
+	private int[] heap;
+	private int maxsize;
+	private int size;
+	public MaxHeap(int maxsize) {
+		this.maxsize=maxsize;
+		heap=new int[this.maxsize+1];
+		heap[0]=Integer.MAX_VALUE;
+		this.maxsize=0;
+	}
+	private int parent(int pos) {
+		return pos/2;
+	}
+	private int leftChild(int pos) {
+		return 2*pos;
+	}
+	private int rightChild(int pos) {
+		return (2*pos)+1;
+	}
+	private void swap(int i, int j) {
+		int temp=heap[i];
+		heap[i]=heap[j];
+		heap[j]=temp;
+	}
+	private boolean isleaf(int pos) {
+		if(2*pos>size) {
+			return true;
+		}
+		return false;
+	}
+	public void insert(int element) {
+		heap[++size]=element;
+		int current= size;
+		while(heap[current]>heap[parent(current)]) {
+			swap(current, parent(current));
+			current=parent(current);
+		}
+		
+	}
+	private void maxHeapify(int pos) {
+		if(isleaf(pos))
+			return;
+		
+		if(heap[pos]<heap[leftChild(pos)] || heap[pos]<heap[rightChild(pos)]) {
+			if(heap[leftChild(pos)]>heap[rightChild(pos)]) {
+				swap(pos,leftChild(pos));
+				maxHeapify(leftChild(pos));
+			}
+			else {
+				swap(pos,rightChild(pos));
+				maxHeapify(rightChild(pos));
+			}
+		}
+	}
+	public int extractMax() {
+		int i =heap[1];
+		heap[1]=heap[size];
+		maxHeapify(1);
+		heap[size]=0;
+		size--;
+		return i;
+		
+	}
+	public void show() {
+		for(int i=1; i<=size/2;i++) {
+			System.out.println("parent="+heap[i]+" leftChild="+heap[leftChild(i)]+" rightChild="+heap[rightChild(i)]);
+		}
+	}
+}
