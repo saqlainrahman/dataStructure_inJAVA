3e71157e395042aef9e0047c390bbad7ad386cb2
Saqlain Afrid Rahman <52929843+saqlainrahman@users.noreply.github.com>
2020-06-29 17:50:41 +0530

    Update QuickSort.java

--- dataStructures/src/dataStructures/QuickSort.java
+++ dataStructures/src/dataStructures/QuickSort.java
@@ -1,13 +1,8 @@
-//not working 
 package dataStructures;
 
 public class QuickSort {
-	private void swap(int i, int j) {
-		int temp=j;
-		j=i;
-		i=temp;
-	}
-	private int partition(int[] arr, int l, int h) {
+	
+	private static int partition(int[] arr, int l, int h) {
 		int pivot= arr[l];
 		
 		int i=l,j=h;
@@ -18,23 +13,51 @@ public class QuickSort {
 			while(arr[j]>pivot) {
 				j--;
 			}
-			if(i<j)
-				swap(arr[i],arr[j]);
+			if(i<j) {
+				int temp= arr[i];
+				arr[i]=arr[j];
+				arr[j]=temp;
+			}
 		}
-		swap(pivot, arr[j]);
+		int temp= arr[j];
+		arr[j]=arr[l];
+		arr[l]=temp;
 		return j;
+		
+		
+		//alternative- high as pivot
+		
+//		int pivot= arr[h];
+//		int i= l-1;
+//		for(int j=l; j<h;j++) {
+//			if(arr[j]<pivot) {
+//				i++;
+//				int temp = arr[i];
+//				arr[i]=arr[j];
+//				arr[j]=temp;
+//			}
+//		}
+//		int temp=arr[i+1];
+//		arr[i+1]=arr[h];
+//		arr[h]=temp;
+//		return i+1;
 	}
-	public void quickSort(int[] arr, int l, int h) {
+	public static void quickSort(int[] arr, int l, int h) {
 		if(l<h) {	
 			int j = partition(arr,l, h);
-			quickSort(arr, l, j);
+			quickSort(arr, l, j-1);
 			quickSort(arr,j+1,h);
 			
 		}	
 	}
-	public void show(int[] arr) {
+	public static void show(int[] arr) {
 		for(int i=0; i<arr.length; i++) {
 			System.out.print(arr[i]+" ");
 		}
+	}
+	public static void main(String[] args) {
+		int arr[] = {2,5,3,1,4,6,56,3,58};
+		quickSort(arr,0,arr.length-1);
+		show(arr);
 	}
 }
